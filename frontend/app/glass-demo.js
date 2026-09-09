b0fa74757afda12523ff0edd568c496458ed29a3
andro <nonfungiblefunyuns@gmail.com>
2026-09-09 04:50:17 -0400

    style: tighten session rows and simplify build icon

--- frontend/app/glass-demo.js
+++ frontend/app/glass-demo.js
@@ -9,7 +9,7 @@
   const dockIcons = {
     crew: '<path d="M5 2h5v5H5zM3 14v-4h9v4M12 3h2v4M14 10h1v4"/>',
     work: '<path d="M3 2h10v12H3zM6 5h4M6 8h4M6 11h2"/>',
-    build: '<path d="M2 4V2h5v3H5v2H2zM6 6l7 7-2 2-7-7M9 3h5v5M11 3v2h3"/>',
+    build: '<path d="M2 2h5v5H2zM2 9h5v5H2zM9 9h5v5H9zM11.5 2v5M9 4.5h5"/>',
     system: '<path d="M4 4h8v8H4zM6 6h4v4H6zM6 1v3M10 1v3M6 12v3M10 12v3M1 6h3M1 10h3M12 6h3M12 10h3"/>'
   };
   Object.entries(dockIcons).forEach(([group, paths]) => {
