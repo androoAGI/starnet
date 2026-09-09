f64c195432cf7725126d1046b238012dd0bcc181
andro <nonfungiblefunyuns@gmail.com>
2026-09-09 12:52:45 -0400

    Simplify Discover app cards and connection details

--- frontend/app/windows/connectors.js
+++ frontend/app/windows/connectors.js
@@ -1110,9 +1110,9 @@
       return '<div class="cc-card' + (e.installed ? ' cc-on' : '') + '" data-id="' + esc(cardId) + '"' + alias +
           ' data-auth="' + esc(e.authType || 'none') + '" data-installed="' + (e.installed ? '1' : '0') + '"' +
           ' style="--ci:' + (ci || 0) + '">' +
-          '<div class="cc-head">' + ccSeal(e) + '<b>' + esc(e.name) + '</b> ' + origin +
-            '<span class="cc-chip" style="color:' + chip[2] + '" title="' + esc(chip[1]) + '">' + (chip[0] ? chip[0] + ' ' : '') + esc(chip[1]) + '</span></div>' +
-          '<div class="cc-blurb dim">' + esc(e.blurb) + '</div>' + presets + (platformMeta ? '<details><summary>Setup details</summary>' + platformMeta + '</details>' : '') + keyField + clientField +
+          '<div class="cc-head">' + ccSeal(e) + '<div class="cc-identity"><b>' + esc(e.name) + '</b>' +
+            '<span class="cc-chip" style="color:' + chip[2] + '" title="' + esc(chip[1]) + '">' + (chip[0] ? chip[0] + ' ' : '') + esc(chip[1]) + '</span></div></div>' +
+          '<div class="cc-blurb dim">' + esc(e.blurb) + '</div>' + clientField + '<details class="cc-details"><summary>Connection details</summary><div class="cc-details-body">' + origin + presets + platformMeta + '</div></details>' + keyField +
           (e.installed ? '<div class="mc-hint">' + (e.releaseDeferred ? 'Saved connection retained. Open Manage Service to view or remove it.' : 'Setup saved. Open Manage Service to check access or reconnect.') + '</div>' : '') +
           '<div class="cc-acts">' + action + home + '</div>' +
         '</div>';
