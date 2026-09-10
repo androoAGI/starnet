0f1ae04e49397ce460c158c97b1202e6315ca2ef
andro <nonfungiblefunyuns@gmail.com>
2026-09-10 01:03:18 -0400

    qa: retain lock heartbeat timeout coercion

--- scripts/qa/closer.mjs
+++ scripts/qa/closer.mjs
@@ -75,7 +75,7 @@ import * as fs from 'node:fs';
 import * as os from 'node:os';
 import * as path from 'node:path';
 import { spawnSync } from 'node:child_process';
-import { runBoundedCommand, npmGateTimeoutMs } from '../lib/run-command.mjs';
+import { runBoundedCommand, coerceTimeoutMs, npmGateTimeoutMs } from '../lib/run-command.mjs';
 import { fingerprintOf } from './ledger.mjs';
 import { isLockStale } from './guardian.mjs';
 
--- scripts/qa/guardian.mjs
+++ scripts/qa/guardian.mjs
@@ -55,7 +55,7 @@ import * as fs from 'node:fs';
 import * as os from 'node:os';
 import * as path from 'node:path';
 import { spawnSync } from 'node:child_process';
-import { runBoundedCommand, npmGateTimeoutMs } from '../lib/run-command.mjs';
+import { runBoundedCommand, coerceTimeoutMs, npmGateTimeoutMs } from '../lib/run-command.mjs';
 import { fingerprintOf, makeLedger } from './ledger.mjs';
 import { runSweep as runEvidenceSweep } from './evidence-sweep.mjs';
 
