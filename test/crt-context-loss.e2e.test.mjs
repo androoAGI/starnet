c417596647264bde00208b1e91dbf29acb7428e2
jazibvirk007-hue <jazib.virk007@gmail.com>
2026-09-20 08:30:26 +0400

    test: harden Chromium CDP binding in context-loss regression

--- test/crt-context-loss.e2e.test.mjs
+++ test/crt-context-loss.e2e.test.mjs
@@ -52,7 +52,8 @@ materializeSeedWorkspace(workspace);
 const sidecar = bootSeededSidecar({ port: appPort, scratchDir: workspace });
 const chrome = spawn(findChrome(), [
   '--headless=new', '--no-sandbox', '--disable-dev-shm-usage', '--no-first-run', '--no-default-browser-check', '--hide-scrollbars', '--mute-audio',
-  '--use-angle=swiftshader', '--enable-unsafe-swiftshader',
+  '--use-angle=swiftshader', '--enable-unsafe-swiftshader', '--disable-gpu-sandbox',
+  '--remote-debugging-address=127.0.0.1', '--disable-background-networking', '--disable-component-update',
   `--remote-debugging-port=${cdpPort}`, '--window-size=1280,720', `--user-data-dir=${profile}`, 'about:blank'
 ], { stdio: 'ignore' });
 
